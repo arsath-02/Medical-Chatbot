c20522ae1cbaadac6e92aafd77213da8e3700aef
RAGAV-24 <ragavrunner33@gmail.com>
2025-02-26 16:15:55 +0530

    ragav changed voice componenet

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FaArrowRight } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-  const navigate=useNavigate();
-  const handleclick =() =>{
-  navigate('/chatbot');<ZASQW> '
-  </ZASQW>
-  }
+  const navigate = useNavigate();
+  const handleclick = () => {
+    navigate('/chatbot');
+  };
+
   return (
     <div className="w-full h-screen flex justify-center items-center relative">
       <iframe
@@ -15,12 +16,21 @@ const HomePage = () => {
         frameBorder="0"
         allowFullScreen
       ></iframe>
-     <button className="absolute flex items-center justify-between px-6 py-3 w-50 bg-blue-500 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-600 transition" onClick={handleclick}>
-        <span className="text-left">GET STARTED</span>
-        <span className=" flex justify-center">
-          <FaArrowRight />
-        </span>
-      </button>
+
+      <div className="absolute flex flex-col items-center gap-4">
+        <h1 className="text-9xl font-bold text-black text-center">
+          MEDI
+        </h1>
+        <button
+          className="flex items-center justify-between px-6 py-3 w-50 bg-blue-500 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-600 transition"
+          onClick={handleclick}
+        >
+          <span className="text-left">GET STARTED</span>
+          <span className="flex justify-center">
+            <FaArrowRight />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
