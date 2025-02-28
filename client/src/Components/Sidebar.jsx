0c3d0832f99b06f863674caa8dcf83aa1bccb770
RAGAV-24 <ragavrunner33@gmail.com>
2025-02-28 09:48:24 +0530

    add navigate to side bar

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -30,6 +30,9 @@ export default function Sidebar({ handleRefreshChat, handleToggleChatHistory, ha
   const handlechatbot=()=>{
     navigate("/chatbot");
   }
+  const handlehealth=()=>{
+    navigate("/FitbitLogin");
+  }
 
 
   return (
@@ -66,7 +69,7 @@ export default function Sidebar({ handleRefreshChat, handleToggleChatHistory, ha
       </div>
       <div className="p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors">
         <div className="ml-1">
-          <MdHealthAndSafety color="gray" size={20} onClick={handlechatbot} />
+          <MdHealthAndSafety color="gray" size={20} onClick={handlehealth} />
         </div>
       </div>
 
